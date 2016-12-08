e3e1a12b960a604dabb6512d0665f741ddfcf9cf
Wilson Berkow <wilsonberkow@gmail.com>
2016-12-08 12:06:34 -0500

    Route / to home.html and route /auth to Kenneth's test page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import dbutil
 from api import reddit, bluemix
 
@@ -19,6 +19,10 @@ def postParam(name):
   return None
 
 @app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/auth')
 def default():
   return '<a href=%s>Authenticate</a>' % reddit.authURL('read')
 
@@ -27,14 +31,14 @@ def reddit_callback():
   error = getParam('error')
   code = getParam('code')
   state = getParam('state')
-  
+
   if error:
     raise('Error: ' + error)
 
   token = reddit.getToken(code)
   print token
   return 'Token: ' + str(token)
-  
+
 if __name__ == '__main__':
   app.debug = True
   reddit.REDIRECT_URI = 'http://127.0.0.1:5000/reddit_callback'
