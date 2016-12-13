a1f47b921fc4abc2bf18a889a8f9abae5c6fc027
Wilson Berkow <wilsonberkow@gmail.com>
2016-12-12 22:05:53 -0500

    Return nice error message if subreddit doesnt exist (instead of crashing)

--- app.py
+++ app.py
@@ -9,20 +9,37 @@ app = Flask(__name__)
 def home():
     if "sub1" in request.args and "sub2" in request.args:
         try:
-            post1 = reddit.getSubredditPosts(request.args["sub1"])[0]
-            post1url = "http://reddit.com" + post1[u'permalink']
-            post2 = reddit.getSubredditPosts(request.args["sub2"])[0]
-            post2url = "http://reddit.com" + post2[u'permalink']
-            # TODO: mash and pass to template
-            return render_template("home.html",
-                    post1title=post1["title"],
-                    post1url=post1url,
-                    post2title=post2["title"],
-                    post2url=post2url)
+            sub1 = reddit.getSubredditPosts(request.args['sub1'])
+            sub2 = reddit.getSubredditPosts(request.args['sub2'])
         except reddit.APIError:
             return redirect('auth')
+        post1url = None
+        post2url = None
+        post1title = None
+        post2title = None
+        no_posts = False # False for success, 'sub1' for no posts in sub1, 'sub2' for no posts in sub2
+        if len(sub1) == 0:
+            no_posts = 'sub1'
+        elif len(sub2) == 0:
+            no_posts = 'sub2'
+        else:
+            post1 = sub1[0]
+            post2 = sub2[0]
+            post1title = post1[u'title']
+            post1url = "http://reddit.com" + post1[u'permalink']
+            post2title = post2[u'title']
+            post2url = "http://reddit.com" + post2[u'permalink']
+        # TODO: mash and pass to template
+        return render_template('home.html',
+                no_posts=no_posts,
+                post1title=post1title,
+                post1url=post1url,
+                post2title=post2title,
+                post2url=post2url,
+                sub1=request.args['sub1'],
+                sub2=request.args['sub2'])
     else:
-        return render_template("home.html")
+        return render_template("home.html", no_posts=False)
 
 @app.route('/auth')
 def auth():
